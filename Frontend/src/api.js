b3e0d97d42ea65e88e75add4975c3afdd7353831
Fiizza <fizzaakram24@gmail.com>
2026-06-30 23:33:32 +0500

    Updated

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,13 +2,14 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+console.log("API_URL =", API_URL);
+
 const api = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 export const uploadPDF = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
